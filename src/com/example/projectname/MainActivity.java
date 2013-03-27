5edfed851728674a3b9cbb43538e0cdda0dc4827
TommyR22 <ruscica@hotmail.it>
2013-03-27 18:12:06 +0100

    Update MainActivity.java

--- src/com/example/projectname/MainActivity.java
+++ src/com/example/projectname/MainActivity.java
@@ -1,1 +1,120 @@
-mamaaka
+package com.example.projectname;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity implements LocationListener {
+    private GoogleMap map;
+    private LocationManager locationManager;
+    private String provider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabledGPS = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean enabledWiFi = service.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        // Check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to 
+        // go to the settings
+        if (!enabledGPS) {
+            Toast.makeText(this, "GPS signal not found", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            Toast.makeText(this, "Selected Provider " + provider,Toast.LENGTH_SHORT).show();
+            onLocationChanged(location);
+        } else {
+
+            //do something
+        }
+
+    }
+
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(provider, 1000, 1, this);
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat =  location.getLatitude();
+        double lng = location.getLongitude();
+        
+        map.clear(); //Remove all marker from map before refresh it.
+        
+        LatLng latLng = new LatLng(lat, lng);
+        
+        Toast.makeText(this, "Location " + lat+","+lng,Toast.LENGTH_SHORT).show();
+        LatLng coordinate = new LatLng(lat, lng);
+        Toast.makeText(this, "Location " + coordinate.latitude+","+coordinate.longitude,Toast.LENGTH_SHORT).show();
+        
+        Marker marker = map.addMarker(new MarkerOptions().position(coordinate).title("Title").snippet("Snippet").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+        
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        // Zoom in the Google Map
+        map.animateCamera(CameraUpdateFactory.zoomTo(18));
+        
+    }
+
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
